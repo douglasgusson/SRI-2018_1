c66eba472b6fd5bce6f6e00bdd0a9c94387f57b3
Douglas Gusson <douglasgusson@gmail.com>
2018-03-09 11:24:58 +0000

    add variaveis.sh

---
+++ variaveis.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Primeiro argumento.... $1"
+echo "Segundo argumento..... $2"
+echo "Todos os argumentos... $*"
+echo "Número de argumentos.. $#"
+echo "PID do processo....... $$"
+echo "Valor saída........... $?"

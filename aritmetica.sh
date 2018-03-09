b1551601be80d787a37732aa4d27cf5cfb09f7e5
Douglas Gusson <douglasgusson@gmail.com>
2018-03-09 12:09:22 +0000

    mudanca de expr para bc

--- aritmetica.sh
+++ aritmetica.sh
@@ -4,8 +4,8 @@ read num1
 echo "Informe outro número:"
 read num2
 echo
-echo "Soma: `expr $num1 + $num2`"
-echo "Subtração `expr $num1 - $num2`"
-echo "Multiplicação: `expr $num1 \* $num2`"
-echo "Divisão: `expr $num1 / $num2`"
+echo "Soma: `echo "$num1+$num2" | bc`"
+echo "Subtração `echo "$num1-$num2" | bc`"
+echo "Multiplicação: `echo "$num1*$num2" | bc`"
+echo "Divisão: `echo "scale=2; $num1/$num2" | bc`"
 echo

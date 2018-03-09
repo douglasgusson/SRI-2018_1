d8c9b76a39960a3ca129b78fcd2c8445ee864dfd
Douglas Gusson <douglasgusson@gmail.com>
2018-03-09 11:50:52 +0000

    adicao de outras operacoes

--- aritmetica.sh
+++ aritmetica.sh
@@ -3,4 +3,9 @@ echo "Informe um número:"
 read num1
 echo "Informe outro número:"
 read num2
+echo
 echo "Soma: `expr $num1 + $num2`"
+echo "Subtração `expr $num1 - $num2`"
+echo "Multiplicação: `expr $num1 \* $num2`"
+echo "Divisão: `expr $num1 / $num2`"
+echo
